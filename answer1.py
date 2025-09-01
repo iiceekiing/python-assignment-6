fd588e3131c3366e6d78b4b39d677500836aebde
iiceekiing <iiceekiingsocials@gmail.com>
2025-09-01 09:52:27 +0100

    add: task 1 and 2 solved

---
+++ answer1.py
@@ -0,0 +1,14 @@
+"""
+    Task 1:
+    Write a function that accepts two numbers (a and b) as parameters
+    and returns their sum.
+    Test the function by calling it with different numbers.  
+"""
+
+
+def task1_sum_of_two_numbers(a, b):
+    return a + b
+
+summation = task1_sum_of_two_numbers(5, 6)
+
+print(summation)
---
+++ answer2.py
@@ -0,0 +1,12 @@
+"""
+    Task 2:
+    Write a function that accepts a number and returns its square.
+    Example: square_number(5) → 25
+"""
+
+def task2_square_number(n):
+    return n * n
+
+square_result = task2_square_number(5)
+
+print(square_result)
